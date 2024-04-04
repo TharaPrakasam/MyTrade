9b849e7f27be2479bc05f790b249dde4fb4567af
TharaPrakasam <45626693+TharaPrakasam@users.noreply.github.com>
2024-04-04 11:44:26 +0200

    Add files via upload

--- static-data.component.ts
+++ static-data.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { SnackbarService } from '../shared/services/snackbar.service';
 @Component({
   selector: 'app-static-data',
   templateUrl: './static-data.component.html',
@@ -13,17 +13,28 @@ export class StaticDataComponent {
   public fxFlexForCol2: any = 50;
   public isHideFirstCol: boolean = true;
   columnDefs = [{ field: "Client Name" }, { field: "Config Type" }, { field: "Created By" }, { field: "Configuration Date"}];
-
+  columnHeader = [
+    { field: "Origin" }, { field: "Modification" }, { field: "User" }, { field: "Date"}
+  ]
   rowData = [
     // { make: "Toyota", model: "Celica", price: 35000 },
     // { make: "Ford", model: "Mondeo", price: 32000 },
     // { make: "Porsche", model: "Boxter", price: 72000 }
   ];
 
-  constructor() {
+  rowData1 = [
+    {origin:'',modification: 'Test',user:'G1-45124',Date:'27/03/2024 07:00:45'}
+  ];
+  constructor(
+    public snackBarService : SnackbarService
+  ) {
     this.innerHeight = window.innerHeight - 90;
     this.btnHeight = window.innerHeight - 560;
 
+  }
+
+  ngOnInit() {
+    this.snackBarService.showSnackbar('Show Error Message Show Error MessageShow Error MessageShow Error Message Show Error Message','close','100000');
   }
 
   fullscreen() {
